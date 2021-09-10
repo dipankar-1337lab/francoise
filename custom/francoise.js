1bf597cbe845c126730a34740c4e273d65354595
Dipankar - 1337 Lab <53222413+dipankar-1337lab@users.noreply.github.com>
2021-09-10 18:48:48 +0530

    Changes for 10th Sept

--- custom/francoise.js
+++ custom/francoise.js
@@ -16,13 +16,13 @@
 })();
 
 
- 
-var closeMenuG =  document.getElementsByClassName('naver');
 
-Array.prototype.forEach.call(closeMenuG, function(element) {
-  element.addEventListener('click', function() {
-       document.querySelector('.b2c-collapse').classList.remove('open')
-       document.querySelector('.b2b-collapse').classList.remove('open')  
+var closeMenuG = document.getElementsByClassName('naver');
+
+Array.prototype.forEach.call(closeMenuG, function (element) {
+  element.addEventListener('click', function () {
+    document.querySelector('.b2c-collapse').classList.remove('open')
+    document.querySelector('.b2b-collapse').classList.remove('open')
     document.querySelector('.offcanvas-collapse').classList.remove('open')
   });
 });
@@ -31,14 +31,15 @@ Array.prototype.forEach.call(closeMenuG, function(element) {
 
 (function () {
   'use strict'
- document.querySelector('#leftMenu').addEventListener('click', function () {
-    document.querySelector('.b2c-collapse').classList.toggle('open')
+  document.querySelector('#leftMenu').addEventListener('click', function () {
+    document.querySelector('.b2c-collapse').classList.toggle('open');
+    document.querySelector('.navbar-brand').classList('logoSmall')
   })
 })();
- 
+
 (function () {
   'use strict'
-   document.querySelector('#rightMenu').addEventListener('click', function () {
+  document.querySelector('#rightMenu').addEventListener('click', function () {
     document.querySelector('.b2b-collapse').classList.toggle('open')
   })
 })();
@@ -46,13 +47,31 @@ Array.prototype.forEach.call(closeMenuG, function(element) {
 ////// close service type ////
 (function () {
   'use strict'
-   document.querySelector('#closer').addEventListener('click', function () {
+  document.querySelector('#closer').addEventListener('click', function () {
     document.querySelector('.b2b-collapse').classList.remove('open')
   })
 })();
 (function () {
   'use strict'
-   document.querySelector('#closer2').addEventListener('click', function () {
+  document.querySelector('#closer2').addEventListener('click', function () {
     document.querySelector('.b2c-collapse').classList.remove('open')
   })
 })();
+
+////// Logo on Scroll //////
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 1550) {
+	  
+    $('.navbar').addClass('fixbar');
+    $('.logo').addClass('logoSmall');
+  } else {
+    $('.logo').removeClass('logoSmall');
+    $('.navbar').removeClass('fixbar');
+  }
+});
+
+var carousel = $('.testimonials .carousel-item');
+for (var i = 0; i < carousel.length; i++) {
+  var dataBg = $(carousel[i]).attr('data-image')
+  $(carousel[i]).css('background-image', 'url(' + dataBg + ')')
+}
